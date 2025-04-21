89c0a693834b64a205c711229aebb7e69896427d
cdjohnson97 <cdmilandou@gmail.com>
2025-04-21 08:42:39 +0200

    new changes on main

--- src/main/java/com/todo/user_api_demo/UserApiDemoApplication.java
+++ src/main/java/com/todo/user_api_demo/UserApiDemoApplication.java
@@ -8,7 +8,7 @@ public class UserApiDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApiDemoApplication.class, args);
-        System.out.println("Lancement avec succèss avec github actions !...");
+        System.out.println("Application lancée avec succèes depuis la dernière jour !...");
     }
 
 }
